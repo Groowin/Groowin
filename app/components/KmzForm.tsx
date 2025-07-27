6058821cdf16ddc16ed80332eaf34bc200031592
Groowin <askerovalmas50@gmail.com>
2025-07-27 21:41:58 +0500

    Create KmzForm.tsx

---
+++ app/components/KmzForm.tsx
@@ -0,0 +1,10 @@
+export function KmzForm() {
+  return (
+    <form>
+      <label>Пән</label><input name="subject" />
+      <label>Сынып</label><input name="grade" />
+      <label>Тақырып</label><input name="topic" />
+      <button>ҚМЖ жаса</button>
+    </form>
+  );
+}
